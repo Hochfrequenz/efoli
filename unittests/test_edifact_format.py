6d7cc8694cdefbb734e603812368c9d32a0ceccb
konstantin <konstantin.klein@hochfrequenz.de>
2024-07-31 21:22:06 +0200

    🦠Copy code from `maus`; Initial Package Setup (#2)

---
+++ unittests/test_edifact_format.py
@@ -0,0 +1,50 @@
+from typing import Optional
+
+import pytest
+
+from efoli import EdifactFormat, get_format_of_pruefidentifikator
+
+
+@pytest.mark.parametrize(
+    "pruefi, expected",
+    [
+        pytest.param("11042", EdifactFormat.UTILMD),
+        ("13002", EdifactFormat.MSCONS),
+        ("25001", EdifactFormat.UTILTS),
+        ("44001", EdifactFormat.UTILMD),
+        ("55001", EdifactFormat.UTILMD),
+    ],
+)
+def test_pruefi_to_format(pruefi: str, expected: EdifactFormat) -> None:
+    """
+    Tests that the prüfis can be mapped to an EDIFACT format
+    """
+    assert get_format_of_pruefidentifikator(pruefi) == expected
+
+
+@pytest.mark.parametrize(
+    "illegal_pruefi",
+    [None, "", "asdas", "01234"],
+)
+def test_illegal_pruefis(illegal_pruefi: Optional[str]) -> None:
+    """
+    Test that illegal pruefis are not accepted
+    :return:
+    """
+    with pytest.raises(ValueError):
+        get_format_of_pruefidentifikator(illegal_pruefi)  # type:ignore[arg-type] # ok, because this raises an error
+
+
+@pytest.mark.parametrize("pruefi", [pytest.param("10000")])
+def test_pruefi_to_format_not_mapped_exception(pruefi: str) -> None:
+    """
+    Test that pruefis that are not mapped to an edifact format are not accepted
+    """
+    with pytest.raises(ValueError) as excinfo:
+        _ = get_format_of_pruefidentifikator(pruefi)
+
+    assert "No Edifact format was found for pruefidentifikator" in excinfo.value.args[0]
+
+
+def test_str_representation() -> None:
+    assert str(EdifactFormat.UTILMD) == "UTILMD"
